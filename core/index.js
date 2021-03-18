7a2cc61a8dd06e480b34ce716574cac89b234499
songyanan <songyanan@huohua.cn>
2021-03-18 17:12:14 +0800

    feat 虚拟DOM和Diff

--- core/index.js
+++ core/index.js
@@ -1,6 +1,6 @@
 
 import { effectWatch } from './reactive/index.js'
-import { mountElement } from './renderer/index.js'
+import { mountElement, diff } from './renderer/index.js'
 /**
  * 
  * @param {Object} rootComponent  根组件
@@ -16,13 +16,30 @@ export function createApp(rootComponent) {
     mount(rootContainer) {
       //我们的响应式数据
       const context = rootComponent.setup()
+      //一个标志，作为是否是DOM第一次初始化的过程
+      let isMount = false;
+
+      //存储我们新老vnode中的老vnode
+      let prevVnode;
+
       //用effect处理我们的更新视图的逻辑
       effectWatch(() => {
-        //这里的render方法返回一个渲染后的视图对象，最终挂载到根容器上面
-        const vNode = rootComponent.render(context)
-        mountElement(vNode, rootContainer)
-        // rootContainer.append(element)
+        //第一次挂载
+        if (!isMount) {
+          isMount = true
+          //这里的render方法返回一个渲染后的视图对象，最终挂载到根容器上面
+          const vNode = rootComponent.render(context)
+          mountElement(vNode, rootContainer)
+          prevVnode = vNode
+        } else {
+          //后续更新过程
+          const vNode = rootComponent.render(context)
+          diff(prevVnode, vNode)
+          prevVnode = vNode
+        }
+
       })
+
     }
   }
 }
--- core/renderer/index.js
+++ core/renderer/index.js
@@ -1,4 +1,96 @@
 
+
+
+/**
+ * diff算法
+ * @param {Object} oldVnode 老节点
+ * @param {Object} newVnode 新节点
+ */
+export function diff(oldVnode, newVnode) {
+  //标签不一样直接替换
+  if (oldVnode.tag !== newVnode.tag) {
+    oldVnode.el.replaceWith(document.createElement(newVnode.tag))
+  } else {
+    //这个地方是将oldVnode.el中的
+    newVnode.el = oldVnode.el
+    //props不同
+    //新增props、删除props、修改props
+    const { props: newProps } = newVnode
+    const { props: oldProps } = oldVnode
+    if (oldProps && newProps) {
+      Object.keys(newProps).forEach(key => {
+        const newVal = newProps[key]
+        const oldVal = oldProps[key]
+        //如果新老props不同，则修改
+        if (newVal !== oldVal) {
+          oldVnode.el.setAttribute(key, newVal)
+        }
+      })
+    } else if (oldProps) {
+      Object.keys(oldProps).forEach(key => {
+        const newVal = newProps[key]
+        const oldVal = oldProps[key]
+        //如果没有新props，则删除
+        if (!newVal) {
+          oldVnode.el.removeAttribute(key, oldVal)
+        }
+      })
+    } else {
+      Object.keys(newProps).forEach(key => {
+        const newVal = newProps[key]
+        oldVnode.el.addAttribute(key, newVal)
+      })
+    }
+    //修改children
+    const { children: newChildren } = newVnode
+    const { children: oldChildren } = oldVnode
+
+    //如果新的children是字符串
+    if (typeof newChildren === 'string') {
+      if (typeof oldChildren === 'string') {
+        //两者不相等，直接替换
+        if (newChildren !== oldChildren) {
+          newVnode.el.innerText(newChildren)
+        }
+      } else if (Array.isArray(oldChildren)) {
+        //老的children是数组
+        newVnode.el.innerText(newChildren)
+      }
+    } else if (Array.isArray(newChildren)) {//新的节点是数组
+      if (typeof oldChildren === 'string') {
+        //先清空节点
+        newVnode.el.innerText = ''
+        mountElement(newVnode, newVnode.el)
+      } else if (Array.isArray(oldChildren)) {
+        const length = Math.min(newChildren.length, oldChildren.length)
+        for (let index = 0; index < length; index++) {
+          const newVnode = newChildren[index]
+          const oldVnode = newChildren[index]
+          diff(oldVnode, newVnode)
+        }
+
+        if (newChildren.length > length) {
+          //创建新节点
+          for (let index = length; index < newChildren.length; index++) {
+            const newVnode = newChildren[index]
+            mountElement(newVnode)
+          }
+        }
+        //删除节点
+        if (oldChildren.length > length) {
+          for (let index = length; index < oldChildren.length; index++) {
+            const oldVnode = oldChildren[index]
+            newVnode.el.parent.removeChild(oldVnode.el)
+          }
+        }
+      }
+    }
+  }
+
+
+}
+
+
 //虚拟Dom转换成真实的DOM
 /**
  * 
@@ -11,6 +103,7 @@ export function mountElement(vNode, container) {
   const { tag, props, children } = vNode
   //tag
   const element = document.createElement(tag)
+  vNode.el = element
 
   //props
   //props类型比较多，所以props的类型其实
